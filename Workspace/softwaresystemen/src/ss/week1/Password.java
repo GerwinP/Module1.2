4e718e04a7a90877a0e323acf63292507e9f86ef
GerwinP <gerwinputtenstein@gmial.com>
2014-11-10 17:58:55 +0100

    Added week1, javadoc and doc folder. Guest.java and Password.java

---
+++ Workspace/softwaresystemen/src/ss/week1/Password.java
@@ -0,0 +1,27 @@
+package ss.week1;
+
+public class Password {
+	
+	public static final String INITIAL = "WELKOM"; 
+	private String password;
+	
+	public Password(){
+		password = INITIAL;
+	}
+	
+	public boolean acceptable(String suggestion){
+	
+		return false;
+	}
+	
+	public boolean setWord(String oldpass, String newpass){
+		
+		return false;
+	}
+	
+	public boolean testWord(String test){
+		
+		return false;
+	}
+	
+}
